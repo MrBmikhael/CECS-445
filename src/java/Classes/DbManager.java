621f9df916c738696a71e6b745edc85bd4cee7ea
administrator <bishoy@bosha-pc.no-ip.biz>
2018-09-01 12:06:17 -0700

    Initial Commit

---
+++ src/java/Classes/DbManager.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Bishoy M
+ */
+public class DbManager {
+    public Connection getConnection()
+    {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://10.10.10.4:3306/cecs", "cecs", "cecs");
+            return conn;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public String getPasswordHash(String Username)
+    {
+        Connection conn = getConnection();
+        
+        if (conn == null)
+            return null;
+        
+        String q = "SELECT `password`FROM `users` WHERE `username` = \"" + Username + "\";";
+        
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(q);
+            if (rs.first())
+                return rs.getString("password");
+        } catch (SQLException ex) {
+            Logger.getLogger(DbManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,23 @@
+
+<%-- 
+    Document   : index
+    Created on : Sep 1, 2018, 2:15:27 AM
+    Author     : Bishoy M
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <form method="POST">
+            <input name="username" id="username" type="text" placeholder="Username">
+            <input name="password" id="password" type="password" placeholder="Password">
+            <input type="submit" value="Login">
+        </form>
+    </body>
+</html>
